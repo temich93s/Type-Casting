314fddee2d93b4308fc5c8195d036b8c1045e4bc
Temich93s <nil>
2021-10-08 12:30:49 +1000

    Пройдена тема: Определение классовой иерархии для приведения типов

--- Type Casting/main.swift
+++ Type Casting/main.swift
@@ -9,3 +9,42 @@ import Foundation
 
 print("Hello, World!")
 
+
+//MARK: Определение классовой иерархии для приведения типов
+print("\n//Определение классовой иерархии для приведения типов")
+
+class MediaItem {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Movie: MediaItem {
+    var director: String
+    init(name: String, director: String) {
+        self.director = director
+        super.init(name: name)
+    }
+}
+ 
+class Song: MediaItem {
+    var artist: String
+    init(name: String, artist: String) {
+        self.artist = artist
+        super.init(name: name)
+    }
+}
+
+let library = [
+    Movie(name: "Casablanca", director: "Michael Curtiz"),
+    Song(name: "Blue Suede Shoes", artist: "Elvis Presley"),
+    Movie(name: "Citizen Kane", director: "Orson Welles"),
+    Song(name: "The One And Only", artist: "Chesney Hawkes"),
+    Song(name: "Never Gonna Give You Up", artist: "Rick Astley")
+]
+// тип "library" выведен как [MediaItem]
+
+print(type(of: library))
+print(type(of: library[0]))
+print(type(of: library[1]))
